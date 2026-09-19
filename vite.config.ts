70ecf6422fc2e5f0aab1e8637f22d235aac799b8
Ethan Vicencio <evicencio5405@gmail.com>
2026-09-19 06:51:36 -0700

    Change host ip

--- vite.config.ts
+++ vite.config.ts
@@ -58,13 +58,13 @@ export default defineConfig(({ mode }) => {
       }),
     ],
     server: {
-      host: '0.0.0.0',
+      host: '127.0.0.1',
       port: 5173,
       strictPort: true,
       allowedHosts: true,
     },
     preview: {
-      host: '0.0.0.0',
+      host: '127.0.0.1',
       port: 4173,
       strictPort: true,
       allowedHosts: true,
